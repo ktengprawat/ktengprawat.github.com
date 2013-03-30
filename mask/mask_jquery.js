fc55a08e199ca4891e13edd4494c1776e01fe0e6
ktengprawat <imakobay@gmail.com>
2013-03-30 19:28:03 -0400

    final draft

--- mask/mask_jquery.js
+++ mask/mask_jquery.js
@@ -5,4 +5,20 @@ $('.runing').mouseenter(
     function(){$(this).addClass("move");}
 )
 
+
+
+
+
+
+
+
+
+$('.picbright1').hover(function() {
+    $(this).stop(true).fadeTo("slow", 0);
+}, function() {
+    $(this).stop(true).fadeTo("slow", 1);
+});
+
+
+
 </script>
