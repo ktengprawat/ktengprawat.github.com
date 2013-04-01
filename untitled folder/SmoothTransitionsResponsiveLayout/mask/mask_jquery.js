db1c4b95a836816f4e73335591fecbffbfc0d992
ktengprawat <imakobay@gmail.com>
2013-04-01 09:50:25 -0400

    omg

---
+++ untitled folder/SmoothTransitionsResponsiveLayout/mask/mask_jquery.js
@@ -0,0 +1,24 @@
+<script>
+
+
+$('.runing').mouseenter(
+    function(){$(this).addClass("move");}
+)
+
+
+
+
+
+
+
+
+
+$('.picbright1').hover(function() {
+    $(this).stop(true).fadeTo("slow", 0);
+}, function() {
+    $(this).stop(true).fadeTo("slow", 1);
+});
+
+
+
+</script>
